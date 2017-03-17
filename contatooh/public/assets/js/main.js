21463ef7de2e9104c4f995eb98d5b2ffd3de8caa
Getulio Vinicius <getuliovinits@gmail.com>
2017-03-16 21:36:40 -0300

    lista de contatos já com AngularJS

--- contatooh/public/assets/js/main.js
+++
@@ -1,2 +0,0 @@
-angular.module("contatooh",[]);
-// jQuery(function () {});
---
+++ contatooh/public/js/controllers/ContatosController.js
@@ -0,0 +1,25 @@
+angular.module('contatooh').controller('ContatosController', function ($scope) {
+    $scope.total = 0;
+
+    $scope.incrementa = function () {
+        $scope.total++;
+    }
+
+    $scope.contatos = [
+        {
+            '_id': 1,
+            'nome': 'Contato Angular 1',
+            'email': 'cont1@empresa.com'
+        },
+        {
+            '_id': 2,
+            'nome': 'Contato Angular 2',
+            'email': 'cont2@empresa.com'
+        },
+        {
+            '_id': 3,
+            'nome': 'Contato Angular 3',
+            'email': 'cont3@empresa.com'
+        }
+    ];
+});
