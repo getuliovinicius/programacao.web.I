e7eb7a9b5a34634bd9c687ac72406baf4121ca49
Gelúlio Vinicius <getuliovinits@gmail.com>
2017-06-17 18:13:46 -0300

    Capitulo 2 concluído - 'Bem começado, metade feito. - Aristóteles'

---
+++ config/express.js
@@ -0,0 +1,38 @@
+// config/express.js
+
+/**
+ * Requisição de módulos
+ */
+var express = require('express');
+// var home = require('../app/routes/home');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+
+module.exports = function() {
+
+    var app = express();
+
+    /**
+     * Configurações de ambiente
+     */ 
+    app.set('port', 3000);
+
+    /**
+     * Middlewares
+     */
+    // app.use(express.static('./public'));
+    app.set('view engine', 'ejs');
+    app.set('views','./app/views');
+    app.use(bodyParser.urlencoded( { extended: true } ));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
+
+    /**
+     * Carregamento de rotas
+     */
+    // home(app);
+    load('models', {cwd: 'app'}).then('controllers').then('routes').into(app);
+    
+    return app;
+
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+
+var http = require('http');
+
+/**
+ * Versão SEM o express
+ */
+/*http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Sou um servidor criado pelo Node.js!\n');
+}).listen(3000, '127.0.0.1'); */
+
+/**
+ * Versão COM o express
+ */
+var app = require('./config/express')();
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express Server escutando na porta ' + app.get('port'));
+});
